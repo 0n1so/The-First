6f3a3e456a5da8a2e930661feaacb7d3f986c6df
0n1so <0nis000000@gmail.com>
2026-08-29 14:57:58 +0300

    1

---
+++ The-First.py
@@ -0,0 +1,19 @@
+import os 
+import random
+
+
+def profit_margin(revenue, cost):
+    if revenue == 0:
+        return 0
+    profit = revenue - cost
+    margin = (profit / revenue) * 100
+    return round(margin, 2)
+
+result = profit_margin(2, 1.98)
+print(result)
+
+x = profit_margin
+
+random_x = random.randint(1, 3)
+if random_x == 1:
+    os.system('cls' if os.name == 'nt' else 'clear')
