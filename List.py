0ce2d7e2e55e858ba2d051dc71cedfd31ffb6fb3
0n1so <0nis000000@gmail.com>
2026-09-01 22:28:50 +0300

    List.py

---
+++ List.py
@@ -0,0 +1,30 @@
+import sys 
+import os
+import random
+
+secret = random.randint(1, 2)
+
+atempt = 0
+guessed = False
+while not guessed:
+    n = int(input("Guess the number (1, 2): "))
+    
+    if n == secret:
+        print("Good")
+        guessed = True
+        
+        list = [ ]
+        s = int(input("Enter the number of elements: "))
+        i = 0
+        while i < s:
+            element = input("Write what you want #" + str(i + 1) + ": ")
+            list.append(element)
+            i += 1
+        print("Your list:", list, "Length:", len(list), "Size:", sys.getsizeof(list), "bytes")
+    else:
+        atempt += 1
+        print("Bad guess. Try again.")
+        if atempt >= 3:
+            print("noob")
+            os.system('cls' if os.name == 'nt' else 'clear')
+            sys.exit()
