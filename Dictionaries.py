331015808f449f4b3e811fa1aa5310d9b5b761cf
0n1so <0nis000000@gmail.com>
2026-09-05 02:33:10 +0300

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,18 @@
+\
+
+
+
+def calculate_lye(oils, superfat=0.05):
+    total_naoh = sum(weight * sap for weight, sap in oils.values())
+    total_naoh *= (1 - superfat)
+    return round(total_naoh, 2)
+
+
+
+oils = {
+    "оливкова": (300, 0.134),
+    "кокосова": (200, 0.178),
+    "касторова": (50, 0.128)
+}
+
+print(calculate_lye(oils, superfat=0.05))
